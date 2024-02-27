60bb6934fe1b2c2defbd99c919d73fe7e7241887
Pure <111236541+PureStupid@users.noreply.github.com>
2024-02-27 16:53:54 +0800

    Update db-connection.js

--- backend-api/routes/db-connection.js
+++ backend-api/routes/db-connection.js
@@ -1,25 +1,18 @@
-// database connection configuration
-const host = "localhost";
-const port = "3306";
-const user = "root";
-const password = "password";
-const database = "e-commerce";
-
-// DO NOT MODIFY ANYTHING BELOW THIS LINE
+const CONFIG = require("../config.json");
 const mysql2 = require("mysql2");
-const  connection = mysql2.createConnection({
-    "host": host,
-    "port": port,
-    "user": user,
-    "password": password,
-    "database": database
+
+const connection = mysql2.createConnection({
+  host: CONFIG.host,
+  port: CONFIG.port,
+  user: CONFIG.user,
+  password: CONFIG.password,
+  database: CONFIG.database,
 });
 
-//Test Connection
-connection.connect(err => {
-    if (err) throw err;
-    console.log('Connected To DB');
-    });
-    // Export the connection so that it can be used
-    // by others script
-    module.exports = connection;
+// Test Connection
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected To DB");
+});
+
+module.exports = connection;
