e7e5ab0cc303915f092f12c73236403987bb5bc2
cxnub <2302241A@student.tp.edu.sg>
2024-02-28 18:23:09 +0800

    Changed to typescript

--- backend-api/src/app.ts
+++ backend-api/src/app.ts
@@ -1,37 +1,21 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-const cors = require("cors");
+import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import authRouter from "./auth/routes";
 
-var app = express();
+dotenv.config();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+const app: Express = express();
+const port = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use("/auth", authRouter);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
-
-module.exports = app;
