db6c2ed921b2c4de79f19c95b74bbde5061530fe
cxnub <2302241A@student.tp.edu.sg>
2024-02-28 18:22:11 +0800

    Update db-connection.ts

--- backend-api/src/database/db-connection.ts
+++ backend-api/src/database/db-connection.ts
@@ -1,23 +1,27 @@
-import mysql2 from 'mysql2/promise';
 import dbConfig from '../config/db.config';
+import {createPool, Pool} from 'mysql2/promise';
 
-const pool = mysql2.createPool({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+var globalPool: Pool | undefined = undefined;
 
-// test the connection
-pool.query('SELECT 1 + 1 AS solution', (error, results, _fields) => {
-  if (error) {
-    console.error("Error connecting to MySQL: " + error.stack);
-    return;
+async function connect(): Promise <Pool> {
+
+  // If the pool was already created, return it instead of creating a new one.
+  if(typeof globalPool !== 'undefined') {
+    return globalPool;
   }
-  console.log("Connected to MySQL as id " + results[0].solution);
-});
 
-export default pool;
+  // If we have gotten this far, the pool doesn't exist, so lets create one.
+  globalPool = await createPool({
+    host: dbConfig.HOST,
+    port: dbConfig.PORT,
+    user: dbConfig.USER,
+    password: dbConfig.PASSWORD,
+    database: dbConfig.DB,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
+  return globalPool;
+}
+
+export default connect;
