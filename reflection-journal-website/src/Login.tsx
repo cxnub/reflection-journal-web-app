39b656dc773e2d5a0a593d9bc265aaed2dcd7077
Skyler-volant <2302110H@student.tp.edu.sg>
2024-03-01 19:06:24 +0800

    added buttons and labels for login page,removed unnecessary import from main page and created css for Login page

--- reflection-journal-website/src/Login.tsx
+++ reflection-journal-website/src/Login.tsx
@@ -1,9 +1,19 @@
+import './css/Login.css'
+
 function Login() {
   
     return (
-        <>
-            <p>test</p>
-        </>
+        <div className="Login">
+            <h3>Login</h3>
+            <input type="text" id="email" name="email"  placeholder = "email"></input><br></br>
+            <input type="text" id="password" name="password" placeholder = "password"></input>
+            <hr></hr>
+            <button id ="new_account" onClick={New_Account}>Create Account</button>
+            {/*<button id ="login" onClick={MyButton}>Login</button>*/}
+        </div>
     )
+}
+function New_Account(){
+    window.location.replace('http://localhost:5173/index.html')
 }
 export default Login
