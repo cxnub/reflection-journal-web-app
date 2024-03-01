41405150b64d87ac51501e646c4f5aa26d191860
Pure <111236541+PureStupid@users.noreply.github.com>
2024-03-02 01:30:20 +0800

    Update schemas.ts
    Fix formatting and typo in username schema error message.
    
    Add max character limit of 25 for username schema.

--- backend-api/src/components/auth/schemas.ts
+++ backend-api/src/components/auth/schemas.ts
@@ -1,73 +1,73 @@
 import { Schema } from "express-validator";
 
 export const createUserAccountSchema: Schema = {
-    email: {
-        notEmpty: {
-            errorMessage: "Email is required",
-        },
-        exists: {
-            errorMessage: "Email is required",
-        },
-        isEmail: {
-            errorMessage: "Invalid email",
-        }
+  email: {
+    notEmpty: {
+      errorMessage: "Email is required",
     },
-    password: {
-        exists: {
-            errorMessage: "Password is required",
-        },
-        notEmpty: {
-            errorMessage: "Password is required",
-        },
-        isLength: {
-            errorMessage: "Password must be at least 8 characters long",
-            options: { min: 8 },
-        }
+    exists: {
+      errorMessage: "Email is required",
     },
-    username: {
-        exists: {
-            errorMessage: "Username is required",
-        },
-        notEmpty: {
-            errorMessage: "Username is required",
-        },
-        isLength: {
-            errorMessage: "Password must be at least 4 characters long",
-            options: { min: 4 }
-        }
+    isEmail: {
+      errorMessage: "Invalid email",
     },
-    image_url: {
-        exists: {
-            errorMessage: "Image URL is required",
-        },
-        notEmpty: {
-            errorMessage: "Image URL is required",
-        }
-    }
+  },
+  password: {
+    exists: {
+      errorMessage: "Password is required",
+    },
+    notEmpty: {
+      errorMessage: "Password is required",
+    },
+    isLength: {
+      errorMessage: "Password must be at least 8 characters long",
+      options: { min: 8 },
+    },
+  },
+  username: {
+    exists: {
+      errorMessage: "Username is required",
+    },
+    notEmpty: {
+      errorMessage: "Username is required",
+    },
+    isLength: {
+      errorMessage: "Username must be between 4 to 25 characters long",
+      options: { min: 4, max: 25 },
+    },
+  },
+  image_url: {
+    exists: {
+      errorMessage: "Image URL is required",
+    },
+    notEmpty: {
+      errorMessage: "Image URL is required",
+    },
+  },
 };
 
 export const loginSchema: Schema = {
-    email: {
-        notEmpty: {
-            errorMessage: "Email is required",
-        },
-        exists: {
-            errorMessage: "Email is required",
-        },
-        isEmail: {
-            errorMessage: "Invalid email",
-        }
+  email: {
+    notEmpty: {
+      errorMessage: "Email is required",
     },
-    password: {
-        exists: {
-            errorMessage: "Password is required",
-        },
-        notEmpty: {
-            errorMessage: "Password is required",
-        },
-        isLength: {
-            errorMessage: "Password must be at least 8 characters long",
-            options: { min: 8 }
-        }
-    }
-}
+    exists: {
+      errorMessage: "Email is required",
+    },
+    isEmail: {
+      errorMessage: "Invalid email",
+    },
+  },
+  password: {
+    exists: {
+      errorMessage: "Password is required",
+    },
+    notEmpty: {
+      errorMessage: "Password is required",
+    },
+    isLength: {
+      errorMessage: "Password must be at least 8 characters long",
+      options: { min: 8 },
+    },
+  },
+};
