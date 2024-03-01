7016d526c9b0eb3e11fbfa513832b340aff82afe
Jia Liang <105258029+shishiboi@users.noreply.github.com>
2024-03-01 19:52:22 +0800

    Added the login.html P.S. still looks weird

--- reflection-journal-website/src/Login.tsx
+++ reflection-journal-website/src/Login.tsx
@@ -1,9 +1,46 @@
 function Login() {
-  
-    return (
-        <>
-            <p>test</p>
-        </>
-    )
+  return (
+    <>
+      <div className="container-fluid background-color-lightskin">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="picture-logo">
+              <p>This is place holder text</p>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="login-form rounded bg-brown p-4">
+              <form>
+                <div className="form-group">
+                  <div>
+                    <label className="form-label text-white">Username:</label>
+                  </div>
+                  <div>
+                    <input name="username" className="form-control"></input>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <div>
+                    <label className="form-label text-white">Password:</label>
+                  </div>
+                  <div>
+                    <input name="password" className="form-control"></input>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <a href="#" className="text-white">
+                    Forgot Password?
+                  </a>
+                </div>
+                <button type="submit" className="btn btn-light">
+                  Create Account
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-export default Login
+export default Login;
--- reflection-journal-website/src/main2.tsx
+++ reflection-journal-website/src/main2.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Login from './Login.tsx'
-import './css/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Login from "./Login.tsx";
+import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.createRoot(document.getElementById('login')!).render(
-    <React.StrictMode>
-      <Login />
-    </React.StrictMode>,
-  )
+ReactDOM.createRoot(document.getElementById("login")!).render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>
+);
