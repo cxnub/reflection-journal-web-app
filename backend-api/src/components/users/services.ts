61ee01c173f84ddda0affd449d3ca4b812636a54
Pure <111236541+PureStupid@users.noreply.github.com>
2024-03-02 23:34:47 +0800

    Update services.ts
    Change id type to integer

--- backend-api/src/components/users/services.ts
+++ backend-api/src/components/users/services.ts
@@ -23,11 +23,11 @@ export async function getAllUserProfiles() {
 /**
  * Gets a user profile by id.
  *
- * @param {string} id The user account id
+ * @param {number} id The user account id
  * @returns {Promise<UserProfile | null>} Promise object represents the user profile if found, else `null`
  */
 export async function getUserProfileById(
-  id: string
+  id: number
 ): Promise<UserProfile | null> {
   const conn = await connect();
   const sql = `SELECT * FROM ${dbTableName} WHERE user_account_id = ?`;
@@ -42,18 +42,18 @@ export async function getUserProfileById(
 /**
  * Edits a user profile.
  *
- * @param {string} id The user account id
+ * @param {number} id The user account id
  * @param {UserProfile} data The user profile details
  */
-export async function editUserProfile(id: string, data: UserProfile) {
+export async function editUserProfile(id: number, data: UserProfile) {
   throw Error("Method is not implemented.");
 }
 
 /**
  * Deletes a user profile.
  *
- * @param {string} id The user account id
+ * @param {number} id The user account id
  */
-export async function deleteUserProfile(id: string) {
+export async function deleteUserProfile(id: number) {
   throw Error("Method is not implemented.");
 }
