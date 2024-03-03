4dc9f6c13c08bb254f196e364dcd39e6d244fd37
cxnub <2302241A@student.tp.edu.sg>
2024-03-03 23:33:12 +0800

    Added ssl and proxy

--- reflection-journal-website/vite.config.ts
+++ reflection-journal-website/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/n
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost:3000',
+        changeOrigin: false,
+        secure: false
+      }
+    }
+  }
 })
