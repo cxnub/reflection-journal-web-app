deca656200a08dd557154cce96e42639ac02b4f9
Skyler-volant <2302110H@student.tp.edu.sg>
2024-03-04 14:07:19 +0800

    Finished most of homepage design except profile button

--- reflection-journal-website/src/home/presentation/View/home.tsx
+++ reflection-journal-website/src/home/presentation/View/home.tsx
@@ -7,7 +7,7 @@ import { BiBell } from "react-icons/bi";
 function Home(){
     
     return(
-        <>
+        <div className="Homepage">
             <div className="taskbar">
                 <div className="row">
                     <div className="col-md-3 column_1">
@@ -41,7 +41,89 @@ function Home(){
                     </div>
                 </div>
             </div>
-        </>
+            <div className="Nav_list">
+                <div className="row">
+                    <div className="col-md-3">
+                        <form className="Nav_panel">
+                            <a href="http://localhost:5173/index.html">
+                                <div className="Nav_buttons">Home</div>
+                            </a>
+                            <hr></hr>
+                            <a href="http://localhost:5173/index.html">
+                                <div className="Nav_buttons">Following</div>
+                            </a>
+                            <hr></hr>
+                            <a href="http://localhost:5173/index.html">
+                                <div className="Nav_buttons">My Journals</div>
+                            </a>
+                            <hr></hr>
+                            <a href="http://localhost:5173/index.html">
+                                <div className="Nav_buttons">History</div>
+                            </a>
+                            <hr></hr>
+                        </form>
+                    </div>
+                    <div className="col-md-9">
+                        <form className="discover_journals">
+                            <ul className="grid">
+                                <a href="http://localhost:5173/index.html">
+                                    <li className="journal_entry">
+                                        <div className="journal_preview">
+                                            <h1>Title</h1>
+                                            <hr className="journal_line"></hr>
+                                            <p className="journal_text">Description</p>
+                                        </div>
+                                    </li>
+                                </a>
+                                <a href="http://localhost:5173/index.html">
+                                    <li className="journal_entry">
+                                        <div className="journal_preview">
+                                            <h1>Title</h1>
+                                            <hr className="journal_line"></hr>
+                                            <p className="journal_text">Description</p>
+                                        </div>
+                                    </li>
+                                </a>
+                                <a href="http://localhost:5173/index.html">
+                                    <li className="journal_entry">
+                                        <div className="journal_preview">
+                                            <h1>Title</h1>
+                                            <hr className="journal_line"></hr>
+                                            <p className="journal_text">Description</p>
+                                        </div>
+                                    </li>
+                                </a>
+                                <a href="http://localhost:5173/index.html">
+                                    <li className="journal_entry">
+                                        <div className="journal_preview">
+                                            <h1>Title</h1>
+                                            <hr className="journal_line"></hr>
+                                            <p className="journal_text">Description</p>
+                                        </div>
+                                    </li>
+                                </a>
+                                <a href="http://localhost:5173/index.html">
+                                    <li className="journal_entry">
+                                        <div className="journal_preview">
+                                            <h1>Title</h1>
+                                            <hr className="journal_line"></hr>
+                                            <p className="journal_text">Description</p>
+                                        </div>
+                                    </li>
+                                </a>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div className="contactUs">
+                <form>
+                    <h2>Contact Us</h2>
+                    <p>Email: Johndoe@journalshare.com</p>
+                    <p>Call: +65 12345678</p>
+                </form>
+            </div>
+        </div>
     );
 }
 export default Home;
